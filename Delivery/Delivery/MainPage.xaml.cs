1d3ea3764a37e40ff5f87bf9826c3c787b397b64
AswathiGR <44152778+AswathiGR@users.noreply.github.com>
2021-04-20 09:40:32 +0530

    Add project files.

---
+++ Delivery/Delivery/MainPage.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Delivery.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Delivery
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+
+    {
+        List<food> places;
+        public MainPage()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch(Exception e) 
+            { 
+            }  
+            List<food> places = new List<food>
+            {
+
+            new food
+
+
+            {
+                Image = "rect_1.png",
+                info = "Groceries and Essentials",
+                arrow="arrow.png"
+            },
+            new food
+
+
+            {
+                Image = "rect_21.png",
+                info = "Groceries and Essentials",
+                arrow = "arrow.png"
+            },
+            new food
+
+
+            {
+                Image = "rect_22.png",
+                info = "Groceries and Essentials",
+                arrow = "arrow.png"
+            },
+            new food
+
+
+            {
+                Image = "rect_31.png",
+                info = "Groceries and Essentials",
+                arrow = "arrow.png"
+            },
+            new food
+
+
+            {
+                Image = "rect_32.png",
+                info = "Groceries and Essentials",
+
+                arrow = "arrow.png"
+            },
+            new food
+
+
+            {
+                Image = "rect_41.png",
+                info = "Groceries and Essentials",
+                arrow = "arrow.png"
+            },
+            new food
+
+
+            {
+                Image = "rect_42.png",
+                info = "Groceries and Essentials",
+                arrow = "arrow.png"
+            },
+            new food
+
+
+            {
+                Image = "rect_43.png",
+                info = "Groceries and Essentials",
+                arrow = "arrow.png"
+            },
+            new food
+
+
+            {
+                Image = "rect_44.png",
+                info = "Groceries and Essentials",
+                arrow = "arrow.png"
+            },
+
+
+
+
+
+
+
+
+
+
+            };
+            FoodView.ItemsSource = places;
+        
+        }
+       /* private async void Go(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new View1());
+        }*/
+    }
+  
+}
---
+++ Delivery/Delivery/models/food.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Delivery.models
+{
+    public class food
+    {
+       
+
+        public string Image { get; set; }
+        public string info { get; set; }
+        public string arrow { get; set; }
+
+    }
+}
